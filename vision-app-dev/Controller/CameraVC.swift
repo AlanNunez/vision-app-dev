ba51d5beccbf56f16bc1a7271af31975dbe24c93
Alan Nunez <raul.nunez01@gmail.com>
2019-02-13 08:53:03 -0400

    Adding flashing light snippet

--- vision-app-dev/Controller/CameraVC.swift
+++ vision-app-dev/Controller/CameraVC.swift
@@ -164,3 +164,26 @@ extension CameraVC: AVSpeechSynthesizerDelegate {
         self.spinner.stopAnimating()
     }
 }
+
+/* func toggleFlash() {
+ guard let device = AVCaptureDevice.default(for: AVMediaType.video) else { return }
+ guard device.hasTorch else { return }
+ 
+ do {
+ try device.lockForConfiguration()
+ 
+ if (device.torchMode == AVCaptureDevice.TorchMode.on) {
+ device.torchMode = AVCaptureDevice.TorchMode.off
+ } else {
+ do {
+ try device.setTorchModeOn(level: 1.0)
+ } catch {
+ print(error)
+ }
+ }
+ 
+ device.unlockForConfiguration()
+ } catch {
+ print(error)
+ }
+ } */
